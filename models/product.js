20af59566b6c68e3c4228d6d1d5ad7a12347d0b0
anurag-10-git <anuragmehra2011@gmail.com>
2022-12-06 01:56:25 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path= require('path');
+const routeDir = require('../util/path');
+
+const p = path.join(routeDir,'data','products.json');
+
+const getProductFromFile = (cb) => {
+
+   fs.readFile(p,(err, fileContent)=> {
+     if(err) {
+       cb([]);
+     }else{
+       cb(JSON.parse(fileContent));
+     }
+   })
+
+}
+
+module.exports = class Product {
+    constructor(title, imageUrl , description, price) {
+       this.title = title;
+       this.imageUrl = imageUrl;
+       this.description = description;
+       this.price= price;
+    }
+   
+    save() {
+      this.id = Math.random().toString();
+      getProductFromFile(products =>{
+         products.push(this);
+         fs.writeFile(p,JSON.stringify(products), (err)=>{
+            console.log(err);
+         });
+      });
+    }
+
+    static fetchAll(cb) {
+      getProductFromFile(cb)
+    }
+
+    static findById(id, cb) {
+      getProductFromFile(products => {
+        const product = products.find(p=> p.id === id);
+        cb(product);
+      })
+    }
+}
